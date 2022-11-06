9af16417dbc5b48924e1c95e8c5e08b9cb28ca4a
Logan Meredith <loganmeredit@gmail.com>
2022-11-06 15:46:49 -0700

    Finish cobyla function

--- kokkos_cobyla_impl.hpp
+++ kokkos_cobyla_impl.hpp
@@ -40,7 +40,40 @@ void cobyla(
     ScalarWorkViewType w,
     IntegralWorkViewType iact
 ) {
+    using SizeType = typename ScalarWorkViewType::size_type;
+    SizeType mpp = m+2;
+    SizeType isim = mpp;
+    SizeType isimi = isim + n*n + n;
+    SizeType idatm = isimi + n*n;
+    SizeType ia = idatm + n*mpp + mpp;
+    SizeType ivsig = ia + m*n + n;
+    SizeType iveta = ivsig + n;
+    SizeType isigb = iveta + n;
+    SizeType idx = isigb + n;
+    SizeType iwork = idx + n;
+    SizeType total_size = n*(3*n + 2*m + 11) + 4*m + 6;
 
+    cobylb(
+        n,
+        m,
+        mpp,
+        x,
+        rhobeg,
+        rhoend,
+        // iprint,
+        maxfun,
+        Kokkos::subview(w, Kokkos::make_pair(0, isim)),
+        Kokkos::subview(w, Kokkos::make_pair(isim, isimi)),
+        Kokkos::subview(w, Kokkos::make_pair(isimi, idatm)),
+        Kokkos::subview(w, Kokkos::make_pair(idatm, ia)),
+        Kokkos::subview(w, Kokkos::make_pair(ia, ivsig)),
+        Kokkos::subview(w, Kokkos::make_pair(ivsig, iveta)),
+        Kokkos::subview(w, Kokkos::make_pair(iveta, isigb)),
+        Kokkos::subview(w, Kokkos::make_pair(isigb, idx)),
+        Kokkos::subview(w, Kokkos::make_pair(idx, iwork)),
+        Kokkos::subview(w, Kokkos::make_pair(iwork, total_size)),
+        iact
+    );
 }
 
 #endif // KOKKOS_COBYLA_IMPL_HPP
