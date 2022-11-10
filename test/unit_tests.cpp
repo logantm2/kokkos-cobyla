874be1589c96e882b6ea3268186f0b14ae9c07bd
Logan Meredith <loganmeredit@gmail.com>
2022-11-10 16:04:28 -0700

    Add license to unit tests

--- test/unit_tests.cpp
+++ test/unit_tests.cpp
@@ -1,3 +1,21 @@
+/*
+COBYLA---Constrained Optimization BY Linear Approximation.
+Copyright (C) 1992 M. J. D. Powell (University of Cambridge)
+Copyright (C) 2022 L. T. Meredith
+
+This package is free software; you can redistribute it and/or
+modify it under the terms of the GNU Lesser General Public
+License as published by the Free Software Foundation; either
+version 2.1 of the License, or (at your option) any later version.
+
+This package is distributed in the hope that it will be useful,
+but WITHOUT ANY WARRANTY; without even the implied warranty of
+MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.
+See the GNU Lesser General Public License
+ https://www.gnu.org/copyleft/lesser.html
+for more details.
+*/
+
 #include "kokkos_cobyla.hpp"
 #include <gtest/gtest.h>
 
